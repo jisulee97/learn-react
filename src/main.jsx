dcc0f7502d006890df6340d2a7ca978c013a2603
leejisu <ejeesoo97@gmail.com>
2023-08-09 13:48:42 +0900

    Add : main.jsx
    - React 요소를 재사용하기 위한 함수 작성

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,44 @@ import * as ReactDOM from "react-dom";
 
 //# React 요소 작성 (React API)
 
-// React 요소 작성 (React API)
+/* React 요소란? --------------------------------------------------------------- */
+
+//# React 요소 작성 (React API)
+
+const visualBoldElement = React.createElement("b", null, "어썸(awesome)");
+
+const headlineElement = React.createElement(
+  "h1",
+  {
+    title: "React is Awesome!",
+  },
+  "React는",
+  visualBoldElement,
+  "해~"
+);
+
+const domAbbrElement = React.createElement(
+  "abbr",
+  { title: "Document Object Model" },
+  "DOM"
+);
+
+const uiAbbrElement = React.createElement(
+  "abbr",
+  { title: "User Interface" },
+  "UI"
+);
+
+const descriptionForReactElement = React.createElement(
+  "p",
+  {},
+  "React는 가상 ",
+  domAbbrElement,
+  "을 사용하는 ",
+  uiAbbrElement,
+  "라이브러리입니다."
+);
+
 const appElement = React.createElement(
   /* type */
   "div",
@@ -23,16 +60,11 @@ const appElement = React.createElement(
     "data-type": "React.ReactElement",
   },
   /* ...children -> [child, child, child, ...] */
-  React.createElement(
-    "h1",
-    {
-      title: "React is Awesome!",
-    },
-    "React는",
-    React.createElement("b", null, "어썸(awesome)"),
-    "해~"
-  )
+  headlineElement,
+  descriptionForReactElement
 );
+
+// appElement(rootElement) tree
 console.log(appElement);
 
 //# React 요소 작성 (With JSX)
@@ -44,12 +76,70 @@ const appElementJSX = (
     <h1 title="React is Awesome!">
       React는 <b>어썸(awesome)</b>해~
     </h1>
+    <p>
+      React는 가상 DOM을 사용하는 <abbr title="User interface">UI</abbr>
+      라이브러리 입니다.
+    </p>
   </div>
 );
 
 console.log(appElementJSX);
 
-//# App 함수 컴포넌트 만들기
+/* React 요소를 재사용하기 위한 함수 작성 --------------------------------------------- */
+
+{
+  // const domAbbrElement = React.createElement(
+  //   'abbr',
+  //   { title: 'Document Object Model'},
+  //   'DOM'
+  // );
+
+  // const uiAbbrElement = React.createElement(
+  //   'abbr',
+  //   { title: 'User Interface'},
+  //   'UI'
+  // );
+
+  // - [ ] React 요소를 반환하는 함수 만들기
+  //   1. 함수 작성 createAbbrElement()
+  function createAbbrElement(props, ...children) {
+    return React.createElement("abbr", props, ...children);
+  }
+
+  const domAbbrElement = createAbbrElement(
+    {
+      title: "Document Object Model",
+    },
+    "D",
+    "O",
+    "M"
+  );
+
+  console.log(domAbbrElement);
+
+  const uiAbbrElement = createAbbrElement(
+    {
+      title: "User Interface",
+    },
+    "UI"
+  );
+  console.log(uiAbbrElement);
+
+  // - [ ] 인수를 전달해 재사용 가능하도록 구현
+  //   2. createAbbrElement({ props: { title: '...' } }, '...');
+}
+
+/* React 요소 vs. 컴포넌트 -------------------------------------------------------- */
+
+//@ 리액트에서 우리가 행해야할 규칙
+
+//@ JSX만 사용하세요.
+//@ 컴포넌트 이름은 첫글자가 항상 대문자로
+//@ 그래야 JSX 구문에서 사용 가능하니까
+
+// //# App 함수 컴포넌트 만들기
+
+/* React 요소 트리를 DOM에 렌더링하려면? ---------------------------------------------- */
 
 //# ReactDOM.createRoot() 를 사용해서 DOM 요소를 ReactDOMRoot 객체로 생성
-//# ReactDOMRoot.render() 메서드로 App을 화면에 표시 (렌더링)
+//# ReactDOMRoot.render() 메서드로 App을 화면에 표시(렌더링)
