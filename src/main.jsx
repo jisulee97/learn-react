529b09c31be483b985d091142cfb9b7e9f3595cb
leejisu <ejeesoo97@gmail.com>
2023-08-09 14:01:10 +0900

    Add : main.jsx
    - JSX와 프론트엔드 개발 도구

--- src/main.jsx
+++ src/main.jsx
@@ -195,3 +195,30 @@ console.log(<App />);
 
 // ReactDOM.createRoot() 를 사용해서 DOM 요소를 ReactDOMRoot 객체로 생성
 // ReactDOMRoot.render() 메서드로 App을 화면에 표시(렌더링)
+
+//# reactDom 으로 dom 구성하기
+
+/*
+import ReactDOM from 'react-dom/client';
+
+
+
+const reactDomRoot = ReactDOM.createRoot(
+  document.getElementById('root')
+)
+
+reactDomRoot.render(
+  <div>
+    <h1>React</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum ab animi, labore facilis obcaecati ullam placeat molestiae. Enim, consectetur laudantium.</p>
+    <ul>
+      <li>list item A</li>
+      <li>list item B</li>
+      <li>list item C</li>
+    </ul>
+  </div>
+)
+*/
+
+//# JSX 가 하는 일은 React 요소 생성 -> 마크업 생성
+//@ JSX → 컴파일러(변환기) → React 요소 → ReactDOM → 실제 DOM 요소 생성 → 구조 작성
