c35a1826f178586c8442b299a057f7c52f620230
leejisu <ejeesoo97@gmail.com>
2023-08-14 14:50:51 +0900

    Add : React 상태 관리 (컴포넌트 로컬 상태)
    - useState
    - [state, setState]
    - setState → renderTrigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import RootLayout from './layout/RootLayout';
-import FilterableList from './pages/Practice/FilterableList';
-
-//  {/* 홈 페이지 : index.html */}
-//  {/* 소개 페이지 : about.html */}
-//  {/* 제품 목록록 페이지 : products.html */}
-//  {/* 홈]의뢰 페이지 : contact.html */}
+import Demo from './pages/Demo';
 
 function App() {
   return (
     <div className="App">
       <RootLayout>
-        <FilterableList />
+        <Demo />
       </RootLayout>
     </div>
   );
---
+++ src/components/Switcher/Switcher.jsx
@@ -0,0 +1,25 @@
+import styles from './Switcher.module.css';
+
+function Switcher({
+  on = false,
+  onLabel = '',
+  offLabel = '',
+  size = 'sm', // sm, md, lg
+  ...restProps
+}) {
+  return (
+    <button
+      type="button"
+      className={`${styles.Switcher} ${styles[size]} ${
+        on ? styles.on : ''
+      }`.trim()}
+      {...restProps}
+    >
+      <span className={styles.handle}>{on ? onLabel : offLabel}</span>
+    </button>
+  );
+}
+
+<Switcher />;
+
+export default Switcher;
