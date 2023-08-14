dc9882799fbc513881f07b8b91573611970f48a6
leejisu <ejeesoo97@gmail.com>
2023-08-14 15:17:49 +0900

    Add : 아토믹 컴포넌트 확장(수정)
    - Switcher 컴포넌트( + 레이블 추가)

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -13,13 +13,13 @@ import React from 'react';
     console.log(myName);
   }
 
-  console.log('컴포넌트 렌더링 myName =', myName);
-}
+  // console.log('컴포넌트 렌더링 myName =', myName);
 
-function Demo() {
   // 상태 관리는 페이지 컴포넌트에서 진행
   // React.useState() 훅으로 선언된 상태로 제어
   // on / off 상태 변수 (관리할 데이터 타입은 Boolean)
+  // 리액트가 요구하는 방법대로 상태를 업데이트(변경)하면?
+  // 렌더링 트리거 → 컴포넌트 렌더링 → DOM 커밋 순으로 진행
 
   // const readyState = React.useState(false); // [s, f]
   // const isReady = readyState[0]; // 상태: state
@@ -27,11 +27,10 @@ function Demo() {
 
   // console.log(isReady);
   // console.log(setIsReady);
+}
 
+function Demo() {
   const [isReady, setIsReady] = React.useState(false);
-
-  // 리액트가 요구하는 방법대로 상태를 업데이트(변경)하면?
-  // 렌더링 트리거 → 컴포넌트 렌더링 → DOM 커밋 순으로 진행
 
   const handleToggleReady = () => {
     setIsReady(!isReady);
@@ -42,24 +41,21 @@ function Demo() {
       style={{
         padding: 40,
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'center',
-        columnGap: 8,
+        alignItems: 'flex-end',
+        gap: 8,
       }}
     >
       {/* <p>{myName}</p> */}
       {/* <button type='button' onClick={handleUpdateMyName}>나의 이름을 바꿔줘</button> */}
       {/* Atomic Component → Stateless or Presentational */}
+      <Switcher on={isReady} label="다크 모드" onClick={handleToggleReady} />
       <Switcher
         on={isReady}
-        onLabel="on"
-        offLabel="off"
-        onClick={handleToggleReady}
-      />
-      <Switcher
-        on={isReady}
-        onLabel="on"
-        offLabel="off"
+        onText="on"
+        offText="off"
+        label="리듀스 모션"
         onClick={handleToggleReady}
       />
     </div>
