6ab0372a4b6b2c4269f4a420c875c118c5b64c07
leejisu <ejeesoo97@gmail.com>
2023-08-22 13:01:02 +0900

    Add : useDelete 훅
    - useProducts.js 추가

--- src/pages/ProductEdit.jsx
+++ src/pages/ProductEdit.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useId, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import useProductItem from '@/hooks/useProductItem';
 import Spinner from '@/components/Spinner';
+import { useDelete as useDeleteProduct } from '@/hooks/products/useProducts';
 
 const initialFormState = {
   title: '',
@@ -11,13 +12,17 @@ const initialFormState = {
 
 function ProductEdit() {
   const titleId = useId();
-  const priceId = useId();
   const colorId = useId();
+  const priceId = useId();
 
   const { productId } = useParams();
+  const navigate = useNavigate();
+
   const { isLoading, data } = useProductItem(productId);
 
   const [formState, setFormState] = useState(initialFormState);
+
+  const deleteProduct = useDeleteProduct();
 
   useEffect(() => {
     if (!isLoading && data) {
@@ -36,6 +41,56 @@ function ProductEdit() {
     });
   };
 
+  const handleEditProduct = (e) => {
+    e.preventDefault(); // ← 이유
+
+    // client → server(pb)
+    // Content-Type: application/json
+    fetch(
+      `${
+        import.meta.env.VITE_PB_API
+      }/collections/products/records/${productId}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formState),
+      }
+    )
+      .then(() => {
+        navigate('/products');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleDeleteProduct = () => {
+    const userConfirm = confirm('정..말로 지울건가요? 🥹');
+
+    if (userConfirm) {
+      deleteProduct(productId)
+        .then((response) => {
+          console.log(response);
+          navigate('/products');
+        })
+        .catch((error) => console.error(error));
+
+      // fetch(`${import.meta.env.VITE_PB_API}/collections/products/records/${productId}`, {
+      //   method: 'DELETE'
+      // })
+      // .then(() => {
+      //   // PB에서 지웠다(성공)
+      //   // 제품 목록 페이지로 이동
+      //   navigate('/products');
+      // })
+      // .catch(error => {
+      //   console.error(error);
+      // });
+    }
+  };
+
   if (isLoading) {
     return <Spinner size={120} />;
   }
@@ -43,8 +98,10 @@ function ProductEdit() {
   if (data) {
     return (
       <>
-        <h2 className="text-2xl text-center">{data.title} 수정 폼</h2>
-        <form>
+        <h2 className="text-2xl text-center">
+          {data.title}({data.color}) 수정 폼
+        </h2>
+        <form onSubmit={handleEditProduct}>
           {/* title */}
           <div>
             <label htmlFor={titleId}>타이틀</label>
@@ -57,7 +114,33 @@ function ProductEdit() {
             />
           </div>
           {/* color */}
+          <div>
+            <label htmlFor={colorId}>컬러</label>
+            <input
+              type="text"
+              name="color"
+              id={colorId}
+              value={formState.color}
+              onChange={handleChangeInput}
+            />
+          </div>
           {/* price */}
+          <div>
+            <label htmlFor={priceId}>프라이스</label>
+            <input
+              type="number"
+              name="price"
+              id={priceId}
+              value={formState.price}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div>
+            <button type="submit">수정</button>
+            <button type="button" onClick={handleDeleteProduct}>
+              삭제
+            </button>
+          </div>
         </form>
       </>
     );
