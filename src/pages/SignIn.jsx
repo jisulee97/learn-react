aae685804369a7750e30e359cc4e2855e5fb13ef
leejisu <ejeesoo97@gmail.com>
2023-08-24 14:38:43 +0900

    Add : 회원 탈퇴 코드 추가

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -16,12 +16,7 @@ function SignIn() {
 
     const { email, password } = formState;
 
-    // PocketBase SDK 인증(로그인) 요청
-    const authData = await pb
-      .collection('users')
-      .authWithPassword(email, password);
-
-    console.log(authData);
+    await pb.collection('users').authWithPassword(email, password);
 
     navigate('/');
   };
@@ -33,8 +28,6 @@ function SignIn() {
       [name]: value,
     });
   }, 400);
-
-  // const handleDebounceInput = debounce(handleInput, 500);
 
   return (
     <div>
@@ -75,6 +68,26 @@ function SignIn() {
       </form>
 
       <Link to="/signup">회원가입</Link>
+      <button
+        type="button"
+        className="ml-4"
+        onClick={async () => {
+          if (confirm('뭐가 맘에 안드시죠? 정말 탈퇴할 생각인가요?')) {
+            if (pb.authStore.model) {
+              try {
+                await pb.collection('users').delete(pb.authStore.model.id);
+                console.log('탈퇴 성공');
+              } catch (error) {
+                console.error(error);
+              }
+            } else {
+              console.log('현재 로그인 된 사용자가 없어요.');
+            }
+          }
+        }}
+      >
+        탈퇴
+      </button>
     </div>
   );
 }
