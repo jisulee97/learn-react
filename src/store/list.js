98d38e5006dea5eb098355df8d731c409c789ca8
leejisu <ejeesoo97@gmail.com>
2023-08-28 16:33:38 +0900

    Add : Zustand 상태 공유 실습 자료 준비

--- src/store/list.js
+++ src/store/list.js
@@ -1,19 +1,27 @@
 import { create } from 'zustand';
 
-// store (define state & action)
-const listStore = () => {
-  // return store's state & actions
-  return {
-    // state
-    list: [
-      {
-        id: crypto.randomUUID(),
-        title: 'Zustand는 츄~스탄트로 발음합니다.',
-      },
-    ],
-    // actions
-  };
-};
+export const useListStore = create((set) => ({
+  // Query
+  list: [
+    {
+      id: crypto.randomUUID(),
+      title: 'Zustand는 츄~스탄트로 발음합니다.',
+    },
+  ],
 
-// hook (bind component) ← store (define state & action)
-export const useListStore = create(listStore);
+  // Mutation
+  addItem: (newItemTitle) =>
+    set((state) => ({
+      list: [
+        ...state.list,
+        {
+          id: crypto.randomUUID(),
+          title: newItemTitle,
+        },
+      ],
+    })),
+  deleteItem: (deleteId) =>
+    set((state) => ({
+      list: state.list.filter((item) => item.id !== deleteId),
+    })),
+}));
